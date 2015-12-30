f027f3d73ed17ba1fd2c8e18da449120950b1937
toshinarin <toshinarin@gmail.com>
2015-12-30 22:22:48 +0900

    added 0 padding.

--- public/js/convert_date.js
+++ public/js/convert_date.js
@@ -2,8 +2,12 @@ document.addEventListener("DOMContentLoaded", function () {
     var elems = document.querySelectorAll('.date');
     for (var i = 0, l = elems.length; i < l; i++) {
         var elem = elems[i];
-        var d = new Date(parseInt(elem.innerText, 10))
-        var ds = d.getFullYear() + '/' + (d.getMonth() + 1) + '/' + d.getDate() + ' ' + d.getHours() + ':' + d.getMinutes()
+        var d = new Date(parseInt(elem.innerText, 10));
+        var MM = ('0' + (d.getMonth() + 1)).slice(-2);
+        var DD = ('0' + d.getDate()).slice(-2);
+        var hh = ('0' + d.getHours()).slice(-2);
+        var mm = ('0' + d.getMinutes()).slice(-2);
+        var ds = d.getFullYear() + '/' + MM + '/' + DD + ' ' + hh + ':' + mm
         elem.innerText = ds
     }
 });
